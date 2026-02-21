b958136439dee85a1676705dc218a43e67df005a
Melvin Saúl <msksaul@gmail.com>
2026-02-21 11:59:26 -0600

    workers

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -64,7 +64,7 @@ export default function Home() {
           <p>Introducing Roomify 2.0</p>
         </div>
 
-        <h1>Built bautiful spaces at the spped of thought with Roomify</h1>
+        <h1>Built beautiful spaces at the speed of thought with Roomify</h1>
 
         <p className='subtitle'>
           Roomify is an AI-first design environment that helps you visualize, render, and ship
